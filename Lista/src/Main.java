914525373b9d157206dac0431e55f12901b7b020
Adrian Pesta <adrian_pesta@wp.pl>
2018-12-15 10:01:32 +0100

    lista

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -10,11 +10,13 @@ public class Main {
         lista.addFirst(23);
         lista.addFirst(24);
         lista.addFirst(25);
+        lista.addFirst(26);
 
         lista.addLast(34);
         lista.addLast(35);
         lista.addLast(36);
         lista.addLast(37);
+        lista.addLast(38);
 
         lista.show();
         lista.showReverse();
